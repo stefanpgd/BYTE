26d90ce1ca8ec42154612273ed2152900b97a4d9
Stefan Pijnacker <stefanpijnacker@gmail.com>
2023-01-29 14:31:15 +0100

    Hand movement in orange essence

--- Source/Game/Essences/ExcitementEssence.cpp
+++ Source/Game/Essences/ExcitementEssence.cpp
@@ -1,5 +1,9 @@
 #include "ExcitementEssence.h"
 
+#include "../../Engine/Input.h"
+#include "../../Engine/Utilities.h"
+#include "../../Graphics/Renderer.h"
+
 ExcitementEssence::ExcitementEssence(Transform* playerTransform, SpriteRenderer* eyeRenderer)
 {
 	this->playerTransform = playerTransform;
@@ -13,8 +17,31 @@ ExcitementEssence::ExcitementEssence(Transform* playerTransform, SpriteRenderer*
 void ExcitementEssence::Update(float deltaTime, glm::vec2 directionalInput)
 {
 	Essence::Update(deltaTime, directionalInput);
+
+	float mouseX = Input::GetMousePosition().x;
+	float mouseY = Input::GetMousePosition().y;
+	float screenWidth = Renderer::GetWindowWidth();
+	float screenHeight = Renderer::GetWindowHeight();
+
+	float ndcX = (mouseX / screenWidth) * 2 - 1.0f;
+	float ndcY = (mouseY / screenHeight) * 2 - 1.0f;
+
+	glm::vec3 screenPos = glm::normalize(glm::vec3(ndcX, -ndcY, 0.0f));
+
+	handOffsetLeft = screenPos * aimHandDistance;
+	handTransform.Position = playerTransform->Position + handOffsetLeft;
+
+	handBopTimer += handBopSpeed * deltaTime;
+	idleHandOffset.x = directionalInput.x > 0 ? idleHandX : -idleHandX;
+	idleHandOffset.y = handYOffset + cosf(handBopTimer) * handBopIdle;
+
+	handOffsetRight = Lerp(handOffsetRight, idleHandOffset, 6.0f * deltaTime);
 }
 
 void ExcitementEssence::Draw(Camera* camera)
 {
+	handRenderer->Draw(camera);
+
+	handTransform.Position = playerTransform->Position + handOffsetRight;
+	handRenderer->Draw(camera);
 }
--- Source/Game/Essences/ExcitementEssence.h
+++ Source/Game/Essences/ExcitementEssence.h
@@ -10,5 +10,19 @@ public:
 	virtual void Draw(Camera* camera) override;
 
 private:
+	// Left Hand //
+	glm::vec3 handOffsetLeft;
+	float aimHandDistance = 1.0f;
+
+	// Right Hand // 
+	glm::vec3 handOffsetRight;
+	glm::vec3 idleHandOffset = glm::vec3(-0.85, 0.0f, 0.1);
+	float idleHandX = -0.85f;
+	float handYOffset = -0.15f;
+	float handBopIdle = 0.04f;
+	float handBopTimer = 0.0f;
+	float handBopSpeed = 4.4f;
+
+	// Shooting //
 
 };
